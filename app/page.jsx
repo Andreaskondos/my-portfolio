3d477d26cb385f917b90b2331368a21f9b78f9c5
Andreas Kondos <andreaskonkon@gmail.com>
2024-04-24 22:46:21 +0300

    fix typograph mistake

--- app/page.jsx
+++ app/page.jsx
@@ -38,7 +38,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-cyan-300/0 via-cyan-300/50 to-cyan-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-400 ">
-          An aspring Full-stack developer. My latest creation is the website{" "}
+          An aspiring Full-stack developer. My latest creation is the website{" "}
           <Link
             target="_blank"
             href="https://allaboutmoms.vercel.app"
